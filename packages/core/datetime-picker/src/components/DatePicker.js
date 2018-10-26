b5c73d5f200eff256bbb805a738ed655a92e662b
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2018-10-26 17:29:51 +1100

    Passinglinter

--- packages/core/datetime-picker/src/components/DatePicker.js
+++ packages/core/datetime-picker/src/components/DatePicker.js
@@ -300,7 +300,7 @@ class DatePicker extends Component<Props, State> {
 
   handleInputChange = (inputValue: string, actionMeta: {}) => {
     const { onInputChange } = this.props.selectProps;
-    onInputChange && onInputChange(inputValue, actionMeta);
+    if (onInputChange) onInputChange(inputValue, actionMeta);
     this.setState({ inputValue });
   };
 
