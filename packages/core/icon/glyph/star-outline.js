ebe5447c2475c4378dad10bf734979e8dec347e3
Ben Conolly <ben@thinkmill.com.au>
2018-11-01 15:36:09 +1100

    correct new glyph paths

--- packages/core/icon/glyph/star-outline.js
+++ packages/core/icon/glyph/star-outline.js
@@ -13,12 +13,12 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-var WatchFilledIcon = function WatchFilledIcon(props) {
+var StarOutlineIcon = function StarOutlineIcon(props) {
   return _react.default.createElement(_index.default, _extends({
-    dangerouslySetGlyph: "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" focusable=\"false\" role=\"presentation\"><g fill=\"currentColor\" fill-rule=\"evenodd\"><path d=\"M11.983 15.984a4.005 4.005 0 0 1-4.002-4c0-2.206 1.795-4 4.002-4a4.005 4.005 0 0 1 4.002 4c0 2.206-1.795 4-4.002 4M12 4C6.48 4 2 8.84 2 12c0 3.086 4.577 8 10 8s10-4.914 10-8c0-3.16-4.481-8-10-8\"/><circle cx=\"12\" cy=\"12\" r=\"2\"/></g></svg>"
+    dangerouslySetGlyph: "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" focusable=\"false\" role=\"presentation\"><path d=\"M14.533 13.236l2.462-2.4-3.402-.494-1.521-3.082-1.521 3.082-3.402.494 2.461 2.4-.58 3.388 3.042-1.6 3.042 1.6-.58-3.388zm-2.461 4.048l-3.905 2.053a1 1 0 0 1-1.451-1.054l.745-4.349-3.159-3.08a1 1 0 0 1 .554-1.705l4.366-.635 1.953-3.956a1 1 0 0 1 1.794 0l1.952 3.956 4.366.635a1 1 0 0 1 .555 1.705l-3.16 3.08.746 4.349a1 1 0 0 1-1.45 1.054l-3.906-2.053z\" fill=\"currentColor\"/></svg>"
   }, props));
 };
 
-WatchFilledIcon.displayName = 'WatchFilledIcon';
-var _default = WatchFilledIcon;
+StarOutlineIcon.displayName = 'StarOutlineIcon';
+var _default = StarOutlineIcon;
 exports.default = _default;
