795b131b8a804adecd2fc9fc3ca164a9a81661c1
William Calderipe <wcalderipe@atlassian.com>
2018-11-08 14:20:28 +1100

    Ignore double dashes symbol for special cases
    In order to be more close as possible from the olde editor we'd to cover
    some special cases when dealing with double dashes.
    
    All examples below are not double dashes in the old editor:
    ```
    --foo
    bar--
    (--
    --)
    ```

--- packages/editor/editor-wikimarkup-transformer/src/parser/tokenize/double-dash-symbol.ts
+++ packages/editor/editor-wikimarkup-transformer/src/parser/tokenize/double-dash-symbol.ts
@@ -1,9 +1,45 @@
 import { Token } from './';
 
-export function doubleDashSymbol(input: string): Token {
+const TOKEN_LENGTH = 2;
+const TOKEN_TYPE = 'text';
+
+export function doubleDashSymbol(input: string, position: number): Token {
+  const charAfterToken = input.charAt(position + TOKEN_LENGTH);
+  if (isAlphanumericalOrUnicode(charAfterToken)) {
+    return fallback();
+  }
+
+  const charBeforeToken = input.charAt(position - 1);
+  if (isAlphanumericalOrUnicode(charBeforeToken)) {
+    return fallback();
+  }
+
+  const isWrappedByParenthesis =
+    isParenthesis(charBeforeToken) || isParenthesis(charAfterToken);
+  if (isWrappedByParenthesis) {
+    return fallback();
+  }
+
   return {
-    type: 'text',
+    type: TOKEN_TYPE,
     text: '–',
-    length: 2,
+    length: TOKEN_LENGTH,
+  };
+}
+
+function isAlphanumericalOrUnicode(char: string): boolean {
+  // Check against alphanumerical or unicode without space
+  return /[a-zA-Z0-9\(\)]|[^\u0000-\u007F]/.test(char);
+}
+
+function isParenthesis(char: string): boolean {
+  return /\(|\)/.test(char);
+}
+
+function fallback(): Token {
+  return {
+    type: TOKEN_TYPE,
+    text: '--',
+    length: TOKEN_LENGTH,
   };
 }
