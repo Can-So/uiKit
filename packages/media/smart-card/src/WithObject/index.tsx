4f1156cf32fc07a2a9d61345c7e7575567fceb53
Ivan Demchenko <idemchenko@atlassian.com>
2018-11-09 15:26:51 +1100

    fixed typecheck issues

--- packages/media/smart-card/src/WithObject/index.tsx
+++ packages/media/smart-card/src/WithObject/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Context from '../Context';
 import { Client, ObjectState } from '../Client';
 import { v4 } from 'uuid';
-import { Provider } from '../Provider';
 
 export interface WithObjectRenderProps {
   state: ObjectState;
@@ -99,8 +98,7 @@ export function WithObject(props: WithObjectProps) {
       {clientFromContext => {
         // TODO: Remove the last fallback - this is a temporary workaround for React context not penetrating the <Editor />
         //       https://product-fabric.atlassian.net/browse/ED-5565
-        const client =
-          clientFromProps || clientFromContext || Provider.defaultClient;
+        const client = clientFromProps || clientFromContext;
         if (!client) {
           throw new Error(
             '@atlaskit/smart-card: No client provided. Provide a client like <Card client={new Client()} url=""/> or <Provider client={new Client()}><Card url=""/></Provider>.',
