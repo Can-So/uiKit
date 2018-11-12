2f11682f63835615dc2d7dbd74ed3c11c193ab86
Alex Hixon <ahixon@atlassian.com>
2018-11-12 16:42:00 +1100

    add kitchen sink example

--- packages/editor/editor-core/examples/5-full-page.tsx
+++ packages/editor/editor-core/examples/5-full-page.tsx
@@ -100,7 +100,7 @@ export const SaveAndCancelButtons = props => (
 
 export type State = { disabled: boolean };
 
-const providers = {
+export const providers = {
   emojiProvider: emoji.storyData.getEmojiResource({
     uploadSupported: true,
     currentUser: {
@@ -116,7 +116,7 @@ const providers = {
   macroProvider: Promise.resolve(macroProvider),
 };
 
-const mediaProvider = storyMediaProviderFactory({
+export const mediaProvider = storyMediaProviderFactory({
   includeUserAuthProvider: true,
 });
 
