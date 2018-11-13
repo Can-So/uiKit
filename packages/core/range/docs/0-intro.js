dd3dbf8a5c5aa448324e84df4f6df7cdaeab87c0
Ben Conolly <ben@thinkmill.com.au>
2018-11-13 13:32:58 +1100

    update range component for newness

---
+++ packages/core/range/docs/0-intro.js
@@ -0,0 +1,30 @@
+// @flow
+import React from 'react';
+import { md, Example, Props, code } from '@atlaskit/docs';
+
+export default md`
+  ### Usage
+
+  Component which renders a slider and is a substitute of the native input[range] element
+
+  ${code`import FieldRange from '@atlaskit/range';`}
+
+  The onChange prop provides a way to subscribe to changes in the value.
+
+  ${(
+    <Example
+      packageName="@atlaskit/range"
+      Component={require('../examples/00-basic-example').default}
+      title="Basic"
+      source={require('!!raw-loader!../examples/00-basic-example')}
+    />
+  )}
+
+  ${(
+    <Props
+      props={require('!!extract-react-types-loader!../src/Range')}
+      heading="FieldRange Props"
+    />
+  )}
+
+`;
---
+++ packages/core/range/examples/01-disabled-example.js
@@ -0,0 +1,5 @@
+// @flow
+import React from 'react';
+import Range from '../src';
+
+export default () => <Range value={50} min={0} max={100} step={1} isDisabled />;
