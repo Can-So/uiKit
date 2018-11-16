2187b9af556b358c192f484677b903535622fe25
ttjandra <ttjandra@atlassian.com>
2018-11-16 13:48:47 +1100

    FS-3179 add i18n changes

--- packages/elements/user-picker/src/i18n/et.ts
+++ packages/elements/user-picker/src/i18n/et.ts
@@ -2,5 +2,4 @@
 export default {
   'fabric.elements.user-picker.placeholder': 'Otsi isikut...',
   'fabric.elements.user-picker.multi.remove-item': 'Remove',
-  'fabric.elements.user-picker.single.clear': 'Clear',
 };
--- packages/elements/user-picker/src/i18n/ru.ts
+++ packages/elements/user-picker/src/i18n/ru.ts
@@ -2,5 +2,4 @@
 export default {
   'fabric.elements.user-picker.placeholder': 'Найти пользователя…',
   'fabric.elements.user-picker.multi.remove-item': 'Remove',
-  'fabric.elements.user-picker.single.clear': 'Clear',
 };
