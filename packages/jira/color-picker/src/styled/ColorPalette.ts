939de3cb8ceb799fa921a589bb7ef4ab583f7521
Pavel Vlasov <pvlasov@atlassian.com>
2018-11-19 10:53:06 +1100

    fix types for color palette

--- packages/jira/color-picker/src/styled/ColorPalette.ts
+++ packages/jira/color-picker/src/styled/ColorPalette.ts
@@ -1,17 +1,14 @@
 import styled from 'styled-components';
-import { HTMLAttributes, ComponentClass } from 'react';
 import { gridSize } from '@atlaskit/theme';
 
-export const ColorCardWrapper: ComponentClass<HTMLAttributes<{}>> = styled.div`
+export const ColorCardWrapper = styled.div`
   display: flex;
   margin: 2px;
 `;
 
 const palettePadding = `padding: ${gridSize() / 2}px ${gridSize()}px`;
 
-export const ColorPaletteContainer: ComponentClass<
-  HTMLAttributes<{}>
-> = styled.div`
+export const ColorPaletteContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   ${palettePadding};
