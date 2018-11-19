fcc926d2643f4140694bdf2733dd2cce0b1b12ef
Ajay Narain Mathur <amathur@atlassian.com>
2018-11-19 11:40:57 +1100

    updated the pagination example with onChange event

--- packages/core/pagination/examples/01-basic.js
+++ packages/core/pagination/examples/01-basic.js
@@ -1,5 +1,27 @@
 //@flow
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import Pagination from '../src';
 
-export default () => <Pagination pages={[...Array(10)].map((_, i) => i + 1)} />;
+const Pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+type StateType = {
+  onChangeEvent: number,
+};
+
+export default class extends Component<{}, StateType> {
+  state = {
+    onChangeEvent: 1,
+  };
+
+  handleChange = (event: SyntheticEvent<>, newPage: number) =>
+    this.setState({ onChangeEvent: newPage });
+
+  render() {
+    return (
+      <Fragment>
+        <Pagination pages={Pages} onChange={this.handleChange} />
+        <p>Selected page index: {this.state.onChangeEvent}</p>
+      </Fragment>
+    );
+  }
+}
