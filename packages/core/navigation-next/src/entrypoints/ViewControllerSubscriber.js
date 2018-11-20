92fcbf8461113c30faf688b7c9a98b9d6b0af95c
Lucas Lago <lucasbenderlago@gmail.com>
2018-11-20 18:24:08 +1100

    Import ViewControllerSubscriber directly

--- packages/core/navigation-next/src/entrypoints/ViewControllerSubscriber.js
+++ packages/core/navigation-next/src/entrypoints/ViewControllerSubscriber.js
@@ -2,6 +2,6 @@
 
 // All entrypoints will be replaced with start with `./dist/esm/` instead of `..` folder since they're connected to the bundle
 
-import { ViewControllerSubscriber } from '../view-controller';
+import ViewControllerSubscriber from '../view-controller/ViewControllerSubscriber';
 
 export default ViewControllerSubscriber;
