b1116f93f2e3a5e4e44ea5ab15bf0185d59848b4
Jared Crowe <jcrowe@atlassian.com>
2018-11-20 20:02:31 +1100

    test ContendNavigation container caching logic

--- packages/core/navigation-next/src/components/presentational/ContentNavigation/__tests__/unit/index.js
+++ packages/core/navigation-next/src/components/presentational/ContentNavigation/__tests__/unit/index.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import ContentNavigation from '../../index';
 import { transitionDurationMs } from '../../../../../common/constants';
@@ -12,11 +12,7 @@ const defaultProps = {
 
 describe('ContentNavigation', () => {
   it('should not trigger animations on first-page load', () => {
-    const wrapper = shallow(
-      <ContentNavigation {...defaultProps}>
-        <div>Hello world</div>
-      </ContentNavigation>,
-    );
+    const wrapper = shallow(<ContentNavigation {...defaultProps} />);
 
     expect(wrapper.find('Transition').props().timeout).toBe(0);
 
@@ -25,5 +21,17 @@ describe('ContentNavigation', () => {
     expect(wrapper.find('Transition').props().timeout).toBe(
       transitionDurationMs,
     );
+  });
+
+  it('should continue rendering the container view while the container layer animates out', async () => {
+    const Container = () => null;
+    const wrapper = mount(<ContentNavigation {...defaultProps} isVisible />);
+
+    expect(wrapper.find(Container)).toHaveLength(0);
+    wrapper.setProps({ container: Container });
+    expect(wrapper.find(Container)).toHaveLength(1);
+    wrapper.setProps({ container: undefined });
+    // Should continue rendering the Container even though we've unset the prop
+    expect(wrapper.find(Container)).toHaveLength(1);
   });
 });
