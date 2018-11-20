939249661f905aabc9de7871766fe8bb3de9ff3b
Patrick Teen <pteen@atlassian.com>
2018-11-21 09:18:08 +1100

    Make optional theme

--- packages/core/drawer/src/components/with-drawer-theme.js
+++ packages/core/drawer/src/components/with-drawer-theme.js
@@ -3,11 +3,14 @@ import React, { Component, type ComponentType } from 'react';
 import { ThemeProvider } from 'styled-components';
 import defaultDrawerTheme from '../theme/default-drawer-theme';
 
-export default (
-  WrappedComponent: ComponentType<*>,
-  theme: Object,
-) => (props: {}) => (
-  <ThemeProvider theme={theme || defaultDrawerTheme}>
-    <WrappedComponent {...props} />
-  </ThemeProvider>
-);
+export default (WrappedComponent: ComponentType<*>, theme: ?Object) => {
+  return class extends Component<*> {
+    render() {
+      return (
+        <ThemeProvider theme={theme || defaultDrawerTheme}>
+          <WrappedComponent {...this.props} />
+        </ThemeProvider>
+      );
+    }
+  };
+};
