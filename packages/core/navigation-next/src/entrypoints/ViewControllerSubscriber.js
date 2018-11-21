13017ee70588e0f7f2c30e7ee5a86b10b011e084
Lucas Lago <lucasbenderlago@gmail.com>
2018-11-21 13:16:05 +1100

    Remove ViewControllerSubscriber entrypoint

--- packages/core/navigation-next/src/entrypoints/ViewControllerSubscriber.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-// All entrypoints will be replaced with start with `./dist/esm/` instead of `..` folder since they're connected to the bundle
-
-import ViewControllerSubscriber from '../view-controller/ViewControllerSubscriber';
-
-export default ViewControllerSubscriber;
