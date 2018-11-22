d83eb88ce4b0d1890951cfd11e98bf807a22f768
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2018-11-22 11:17:59 +1100

    Some PR feedback addressing

--- packages/media/media-picker/src/popup/reducers/__tests__/unit/removeFileFromRecents.spec.tsx
+++ packages/media/media-picker/src/popup/reducers/__tests__/unit/removeFileFromRecents.spec.tsx
@@ -3,6 +3,87 @@ import { removeFileFromRecents as removeFileFromRecentsAction } from '../../../a
 import { mockState } from '../../../mocks';
 import { State } from '../../../domain';
 
+const extraState: Partial<State> = {
+  uploads: {
+    'some-id': {
+      file: {
+        metadata: {
+          id: 'some-id',
+          mimeType: 'some-type',
+          name: 'some-name',
+          size: 42,
+          upfrontId: Promise.resolve('some-local-upfront-file-id'),
+          userUpfrontId: Promise.resolve(''),
+          userOccurrenceKey: Promise.resolve(''),
+        },
+      },
+      events: [],
+      tenant: {} as any,
+      index: 0,
+      timeStarted: 42,
+      progress: null,
+    },
+    'other-upload-id': {
+      file: {
+        metadata: {
+          id: 'some-other-local-file-id',
+          mimeType: 'some-type',
+          name: 'some-name',
+          size: 42,
+          upfrontId: Promise.resolve('some-other-local-upfront-file-id'),
+          userUpfrontId: Promise.resolve(''),
+          userOccurrenceKey: Promise.resolve(''),
+        },
+      },
+      events: [],
+      tenant: {} as any,
+      index: 0,
+      timeStarted: 42,
+      progress: null,
+    },
+  },
+  recents: {
+    items: [
+      {
+        id: 'some-id',
+        insertedAt: 42,
+        occurrenceKey: 'some-other-occurrence-key',
+        type: 'file',
+        details: {} as any,
+      },
+      {
+        id: 'other-id',
+        insertedAt: 42,
+        occurrenceKey: 'some-other-occurrence-key',
+        type: 'file',
+        details: {} as any,
+      },
+    ],
+  },
+  selectedItems: [
+    {
+      serviceName: 'some-service',
+      mimeType: 'some-type',
+      id: 'some-id',
+      upfrontId: Promise.resolve('some-id'),
+      name: 'some-name',
+      size: 42,
+      date: 44,
+      occurrenceKey: 'some-other-occurrence-key',
+    },
+    {
+      serviceName: 'some-other-service',
+      mimeType: 'some-other-type',
+      id: 'other-id',
+      upfrontId: Promise.resolve('other-id'),
+      name: 'some-other-name',
+      size: 42,
+      date: 44,
+      occurrenceKey: 'some-other-occurrence-key',
+    },
+  ],
+};
+
 describe('removeFileFromRecents reducer', () => {
   let state: State;
   let resultState: State;
@@ -18,84 +99,7 @@ describe('removeFileFromRecents reducer', () => {
     );
     state = {
       ...mockState,
-      uploads: {
-        'some-id': {
-          file: {
-            metadata: {
-              id: 'some-id',
-              mimeType: 'some-type',
-              name: 'some-name',
-              size: 42,
-              upfrontId: Promise.resolve('some-local-upfront-file-id'),
-              userUpfrontId: Promise.resolve(''),
-              userOccurrenceKey: Promise.resolve(''),
-            },
-          },
-          events: [],
-          tenant: {} as any,
-          index: 0,
-          timeStarted: 42,
-          progress: null,
-        },
-        'other-upload-id': {
-          file: {
-            metadata: {
-              id: 'some-other-local-file-id',
-              mimeType: 'some-type',
-              name: 'some-name',
-              size: 42,
-              upfrontId: Promise.resolve('some-other-local-upfront-file-id'),
-              userUpfrontId: Promise.resolve(''),
-              userOccurrenceKey: Promise.resolve(''),
-            },
-          },
-          events: [],
-          tenant: {} as any,
-          index: 0,
-          timeStarted: 42,
-          progress: null,
-        },
-      },
-      recents: {
-        items: [
-          {
-            id: 'some-id',
-            insertedAt: 42,
-            occurrenceKey: 'some-other-occurrence-key',
-            type: 'file',
-            details: {} as any,
-          },
-          {
-            id: 'other-id',
-            insertedAt: 42,
-            occurrenceKey: 'some-other-occurrence-key',
-            type: 'file',
-            details: {} as any,
-          },
-        ],
-      },
-      selectedItems: [
-        {
-          serviceName: 'some-service',
-          mimeType: 'some-type',
-          id: 'some-id',
-          upfrontId: Promise.resolve('some-id'),
-          name: 'some-name',
-          size: 42,
-          date: 44,
-          occurrenceKey: 'some-other-occurrence-key',
-        },
-        {
-          serviceName: 'some-other-service',
-          mimeType: 'some-other-type',
-          id: 'other-id',
-          upfrontId: Promise.resolve('other-id'),
-          name: 'some-other-name',
-          size: 42,
-          date: 44,
-          occurrenceKey: 'some-other-occurrence-key',
-        },
-      ],
+      ...extraState,
     };
     state = removeFileFromRecents(state, removeFromRecents);
     resultState = removeFileFromRecents(state, removeFromLocalUploads);
