aac74656ea5fc45a75eced679d55f19a442d96df
Oscar Wallhult <owallhult@atlassian.com>
2018-11-23 15:58:32 +1100

    Fix editor support + styles

---
+++ packages/editor/editor-common/src/styles/shared/link.ts
@@ -0,0 +1,11 @@
+// @ts-ignore: unused variable
+// prettier-ignore
+import { css, Styles, StyledComponentClass } from 'styled-components';
+
+const linkSharedStyle = css`
+  a.blockLink {
+    display: block;
+  }
+`;
+
+export { linkSharedStyle };
---
+++ packages/editor/editor-core/src/plugins/hyperlink/styles.ts
@@ -0,0 +1,10 @@
+// @ts-ignore: unused variable
+// prettier-ignore
+import { css, Styles, StyledComponentClass } from 'styled-components';
+import { linkSharedStyle } from '@atlaskit/editor-common';
+
+export const linkStyles = css`
+  .ProseMirror {
+    ${linkSharedStyle}
+  }
+`;
