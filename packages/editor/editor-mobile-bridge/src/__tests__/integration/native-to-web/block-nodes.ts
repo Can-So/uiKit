462b70f630c733cc91c69dcfb39150a137272e4e
Nathan Flew <nflew@atlassian.com>
2018-11-23 19:26:45 +1100

    CHANGESET: 597df81d. ED-5819: Enables support for text color

--- packages/editor/editor-mobile-bridge/src/__tests__/integration/native-to-web/block-nodes.ts
+++ packages/editor/editor-mobile-bridge/src/__tests__/integration/native-to-web/block-nodes.ts
@@ -1,31 +1,25 @@
 import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
 import Page from '@atlaskit/webdriver-runner/wd-wrapper';
 import {
-  callNativeBridgeFn,
+  callNativeBridge,
   editor,
   editable,
   getDocFromElement,
-  clearEditor,
-} from './_utils';
+  navigateOrClear,
+  skipBrowsers as skip,
+} from '../_utils';
 
 ['action', 'decision', 'blockquote', 'codeblock', 'panel', 'table'].forEach(
   (node, i) => {
     BrowserTestCase(
       `block-nodes.ts: Inserts ${node} and results in valid ADF`,
-      // FIXME Ideally these would be mobile browsers
-      // Safari & Chrome should suffice for now.
-      { skip: ['ie', 'firefox', 'edge'] },
+      { skip },
       async client => {
         const browser = new Page(client);
 
-        if (i === 0) {
-          await browser.goto(editor.path);
-        } else {
-          await clearEditor(browser);
-        }
-
+        await navigateOrClear(browser, editor.path);
         await browser.waitForSelector(`${editable} > p`);
-        await callNativeBridgeFn(browser, 'insertBlockType', node);
+        await callNativeBridge(browser, 'insertBlockType', node);
 
         const doc = await browser.$eval(editable, getDocFromElement);
         expect(doc).toMatchDocSnapshot();
