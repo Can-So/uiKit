4f01ce36e8ee836b249b3432762c52f49d2bed1f
Will Mendes <willmendesneto@gmail.com>
2018-11-23 12:38:43 +1100

    fix(navigation-next): fixing exports integration on JFE

--- packages/core/navigation-next/src/entrypoints/ui-controller.js
+++ packages/core/navigation-next/src/entrypoints/ui-controller.js
@@ -1,4 +1,10 @@
 // @flow
 
 // All entrypoints will be replaced with start with `./dist/esm/` instead of `..` folder since they're connected to the bundle
-export * from '../ui-controller';
+export { default as UIController } from '../ui-controllerUIController';
+export {
+  default as UIControllerSubscriber,
+} from '../ui-controllerUIControllerSubscriber';
+export {
+  default as withNavigationUIController,
+} from '../ui-controllerwithNavigationUIController';
