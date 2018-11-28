00e838c355c3ed7a7435e26dc13d442ea6b3fa3c
Pete Gleeson <pgleeson@atlassian.com>
2018-11-28 14:35:02 +1100

    fills out basic examples

--- packages/core/form/examples/1-text-fields.js
+++ packages/core/form/examples/1-text-fields.js
@@ -1,1 +1,60 @@
 // @flow
+import React from 'react';
+import Button from '@atlaskit/button';
+import TextField from '@atlaskit/textfield';
+import TextArea from '@atlaskit/textarea';
+import { FieldTextStateless } from '@atlaskit/field-text';
+import { FieldTextAreaStateless } from '@atlaskit/field-text-area';
+import Form, { Field, FormFooter } from '../src';
+
+export default () => (
+  <div
+    style={{
+      display: 'flex',
+      width: '400px',
+      margin: '0 auto',
+      flexDirection: 'column',
+    }}
+  >
+    <Form onSubmit={data => console.log(data)}>
+      {({ formProps }) => (
+        <form {...formProps}>
+          <Field name="firstname" defaultValue="" label="First name" isRequired>
+            {({ fieldProps }) => <TextField {...fieldProps} />}
+          </Field>
+
+          <Field name="lastname" defaultValue="" label="Last name" isRequired>
+            {({ fieldProps: { isRequired, ...others } }) => (
+              <FieldTextStateless
+                isLabelHidden
+                shouldFitContainer
+                required={isRequired}
+                {...others}
+              />
+            )}
+          </Field>
+
+          <Field name="description" defaultValue="" label="Description">
+            {({ fieldProps }) => <TextArea {...fieldProps} />}
+          </Field>
+
+          <Field name="comments" defaultValue="" label="Additional comments">
+            {({ fieldProps }) => (
+              <FieldTextAreaStateless
+                isLabelHidden
+                shouldFitContainer
+                {...fieldProps}
+              />
+            )}
+          </Field>
+
+          <FormFooter>
+            <Button type="submit" appearance="primary">
+              Submit
+            </Button>
+          </FormFooter>
+        </form>
+      )}
+    </Form>
+  </div>
+);
---
+++ packages/core/form/examples/3-select-fields.js
@@ -0,0 +1,113 @@
+// @flow
+import React, { Fragment } from 'react';
+import Select from '@atlaskit/select';
+import Button from '@atlaskit/button';
+import Form, { Field, FormFooter, ErrorMessage, HelperMessage } from '../src';
+
+const colors = [
+  { label: 'blue', value: 'blue' },
+  { label: 'red', value: 'red' },
+  { label: 'purple', value: 'purple' },
+  { label: 'black', value: 'black' },
+  { label: 'white', value: 'white' },
+  { label: 'gray', value: 'gray' },
+  { label: 'yellow', value: 'yellow' },
+  { label: 'orange', value: 'orange' },
+  { label: 'teal', value: 'teal' },
+  { label: 'dog', value: 'dog' },
+];
+
+const flavors = [
+  { label: 'vanilla', value: 'vanilla' },
+  { label: 'strawberry', value: 'strawberry' },
+  { label: 'chocolate', value: 'chocolate' },
+  { label: 'mango', value: 'mango' },
+  { label: 'rum', value: 'rum' },
+  { label: 'hazelnut', value: 'hazelnut' },
+  { label: 'durian', value: 'durian' },
+];
+
+const validateOnSubmit = data => {
+  let errors;
+  errors = colorsValidation(data, errors);
+  errors = flavorValidation(data, errors);
+  return errors;
+};
+
+const colorsValidation = (data, errors) => {
+  if (data.colors) {
+    return data.colors.value === 'dog'
+      ? {
+          ...errors,
+          colors: `${data.colors.value} is not a color`,
+        }
+      : errors;
+  }
+  return errors;
+};
+
+const flavorValidation = (data, errors) => {
+  if (data.icecream && data.icecream.length >= 3) {
+    return {
+      ...errors,
+      icecream: `${
+        data.icecream.length
+      } is too many flavors, don't be greedy, you get to pick 2.`,
+    };
+  }
+
+  return errors;
+};
+
+export default () => (
+  <div
+    style={{
+      display: 'flex',
+      width: '400px',
+      margin: '0 auto',
+      flexDirection: 'column',
+    }}
+  >
+    <Form
+      onSubmit={data => {
+        console.log('form data', data);
+        return Promise.resolve(validateOnSubmit(data));
+      }}
+    >
+      {({ formProps }) => (
+        <form {...formProps}>
+          <Field name="colors" label="Select a colour" defaultValue={[]}>
+            {({ fieldProps, error }) => (
+              <Fragment>
+                <Select
+                  validationState={error ? 'error' : 'none'}
+                  {...fieldProps}
+                  options={colors}
+                />
+                {error && <ErrorMessage>{error}</ErrorMessage>}
+              </Fragment>
+            )}
+          </Field>
+          <Field name="icecream" label="Select a flavor" defaultValue={[]}>
+            {({ fieldProps, error }) => (
+              <Fragment>
+                <Select
+                  validationState={error ? 'error' : 'none'}
+                  {...fieldProps}
+                  options={flavors}
+                  isMulti
+                />
+                {error && <ErrorMessage>{error}</ErrorMessage>}
+              </Fragment>
+            )}
+          </Field>
+          <FormFooter>
+            <Button type="submit" appearance="primary">
+              Submit
+            </Button>
+          </FormFooter>
+        </form>
+      )}
+    </Form>
+  </div>
+);
