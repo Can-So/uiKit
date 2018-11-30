783e84ae9c0f998cdffaae92fff6e4bd51fab25a
Pete Gleeson <pgleeson@atlassian.com>
2018-11-30 11:17:42 +1100

    updates validation docs

--- packages/core/form/docs/4-validation.js
+++ packages/core/form/docs/4-validation.js
@@ -21,18 +21,103 @@ This page covers how validation works at both form and field level.
 <a name="field-validation"></a>
 ## Field-level validation
 
-The
-
-<a name="form-validation"></a>
-## Form validation
-
+A field's value can be validated by using the "validate" prop. This prop accepts
+a function that is called whenever a field value changes. The validation function
+gets passed the current field value and form state. If the validation fails, 
+return the error. Otherwise, return undefined.
 
 ${code`
 import Form, { ErrorMessage, HelperMessage, ValidMessage } from '@atlaskit/form';
 import Button from '@atlaskit/button';
+import getUser from './api/getUser';
 
-const MyField = () => {}
+const validate = value => {
+  if (value.length < 7) {
+    return 'TOO_SHORT';
+  }
+  return getUser(value).then(user => (user ? 'IN_USE' : undefined));
+};
 
+const UsernameField = () => {
+  <Field
+    name="username"
+    label="Password"
+    defaultValue=""
+    isRequired
+    validate={validate}
+  >
+    {({ fieldProps, error }) => (
+      <>
+        <TextField {...fieldProps} />
+        {!error && (
+          <HelperMessage>Should be more than 6 characters</HelperMessage>
+        )}
+        {error === 'TOO_SHORT' && (
+          <ErrorMessage>
+            Invalid password, needs to be more than 6 characters
+          </ErrorMessage>
+        )}
+        {error === 'IN_USE' && (
+          <ErrorMessage>Username already, try another one</ErrorMessage>
+        )}
+      </>
+    )}
+  </Field>;
+};
 `}
+
+If the validation requires an async check the validation function can a Promise. Note
+that the promise should **resolve** the error rather than reject with the error.
+
+<a name="submission-validation"></a>
+## Submission validation
+
+When the form gets submitted, the current state gets passed to the onSubmit handler.
+You communicate submission errors in a similar way to field-level validation. 
+If there was a submission error, the onSubmit handler should return an object. 
+Say there was a problem with the "password" field, the object should contain that 
+key and the error as the value.
+
+If the submit succeeded, the onSubmit handler should return undefined.
+
+${code`
+import Form, { Field } from '@atlaskit/form';
+import TextField from '@atlaskit/textfield';
+import Button from '@atlaskit/button';
+import createUser from './api/createUser';
+
+const MyForm = () => (
+  <Form
+    onSubmit={data =>
+      createUser(data).catch(err => ({ [err.field]: err.value }))
+    }
+  >
+    {({ formProps }) => (
+      <form {...formProps}>
+        <Field name="username" label="Username" defaultValue="">
+          {({ fieldProps, error }) => (
+            <>
+              <TextField {...fieldProps} />
+              {error && <ErrorMessage>{error}</ErrorMessage>}
+            </>
+          )}
+        </Field>
+        <Field name="password" label="Password" defaultValue="">
+          {({ fieldProps, error }) => (
+            <>
+              <TextField type="password" {...fieldProps} />
+              {error && <ErrorMessage>{error}</ErrorMessage>}
+            </>
+          )}
+        </Field>
+        <Button type="submit">Submit</Button>
+      </form>
+    )}
+  </Form>
+);
+`}
+
+OnSubmit can return synchronously or a Promise that resolves to the result. Note that
+the promise should **resolve** the error rather than reject with the error.
 
 `;
