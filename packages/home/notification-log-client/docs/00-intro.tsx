86d27e35aeb6a18c734a2f0b661b898ffa0897cb
raja07 <rbellebon@atlassian.com>
2018-12-03 11:17:28 +1100

    Update docs for Home

--- packages/home/notification-log-client/docs/00-intro.tsx
+++ packages/home/notification-log-client/docs/00-intro.tsx
@@ -1,14 +1,14 @@
-import { md } from '@atlaskit/docs';
+import * as React from 'react';
+import { md, code, Example } from '@atlaskit/docs';
 
 export default md`
   \`notification-log-client\` is a fetch client implementation for making API calls to notification-log service.
 
   This is intended to be used as a provider into other components, such as NotificationIndicator.
 
-  ## Examples
+  ## Usage
 
-  ~~~
-  import { NotificationLogClient } from '@atlaskit/notification-log-client';
+  ${code`import { NotificationLogClient } from '@atlaskit/notification-log-client';
 
   const notificationLogClient = new NotificationLogClient(
     'http://base-url-to-notification-log-service',
@@ -16,6 +16,13 @@ export default md`
   );
 
   const result = await notificationLogClient.countUnseenNotifications();
-  console.log(result.count);
-  ~~~
+  console.log(result.count);`}
+
+  ${(
+    <Example
+      Component={require('../examples/00-basic').default}
+      title="Basic"
+      source={require('!!raw-loader!../examples/00-basic')}
+    />
+  )}
 `;
