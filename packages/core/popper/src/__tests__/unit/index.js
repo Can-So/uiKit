efc35d1e79c7830cb81d9365f7dbfbb98e1e9845
raja07 <rbellebon@atlassian.com>
2018-12-12 10:48:58 +1100

    CHANGESET: 0670d81e. Adding devDep

--- packages/core/popper/src/__tests__/unit/index.js
+++ packages/core/popper/src/__tests__/unit/index.js
@@ -3,6 +3,10 @@ import { mount, shallow } from 'enzyme';
 import React from 'react';
 import { Popper } from '../..';
 
+const Content = () => <div className="content">Hello</div>;
+
+const referenceElement = document.createElement('div');
+
 const mountPopper = props =>
   mount(
     <Popper {...props}>
@@ -14,8 +18,6 @@ const mountPopper = props =>
     </Popper>,
   );
 
-const referenceElement = document.createElement('div');
-
 test('Popper should be defined', () => {
   const wrapper = mountPopper({ referenceElement });
   expect(wrapper).not.toBeNull();
@@ -25,7 +27,15 @@ test('Popper should be pass its children', () => {
   expect(shallow(<Popper />).children()).toHaveLength(1);
 });
 
-test('Popper should render the correct markup', () => {
-  const wrapper = mountPopper({ referenceElement });
-  expect(wrapper).toMatchSnapshot();
+test('should render content into popup', () => {
+  const wrapper = mount(
+    <Popper referenceElement={referenceElement}>
+      {({ ref, style, placement }) => (
+        <div ref={ref} style={style} data-placement={placement}>
+          <Content />
+        </div>
+      )}
+    </Popper>,
+  );
+  expect(wrapper.find(Content)).toHaveLength(1);
 });
