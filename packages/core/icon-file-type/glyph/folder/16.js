928e0bb1a76d0e5402381fcb145d26f2b860fd19
Stanislav Sysoev <ssysoev@atlassian.com>
2018-12-12 09:33:43 +1100

    Remove metadata from icon

--- packages/core/icon-file-type/glyph/folder/16.js
+++ packages/core/icon-file-type/glyph/folder/16.js
@@ -7,14 +7,14 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _index = _interopRequireDefault(require("../../cjs/index"));
+var _Icon = _interopRequireDefault(require("../../cjs/components/Icon"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
 var Folder16Icon = function Folder16Icon(props) {
-  return _react.default.createElement(_index.default, _extends({
+  return _react.default.createElement(_Icon.default, _extends({
     dangerouslySetGlyph: "<svg width=\"16\" height=\"16\" viewBox=\"0 0 16 16\" focusable=\"false\" role=\"presentation\"><g fill=\"#B3D4FF\" fill-rule=\"evenodd\"><path d=\"M6.667 3H15a1 1 0 0 1 1 1v10.05a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4.667a1 1 0 0 1 1 1z\"/><path d=\"M0 4.05h16v10a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1v-10z\" style=\"mix-blend-mode:multiply\"/></g></svg>"
   }, props, {
     size: "small"
