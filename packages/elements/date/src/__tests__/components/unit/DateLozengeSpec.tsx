67d563a460a129f3c83e5ed1cd09ad96ad610858
Jake Coppinger <jcoppinger@atlassian.com>
2018-12-11 11:59:03 +1100

    CHANGESET: 8505afc3. ED-5888 Add dark mode for date

--- packages/elements/date/src/__tests__/components/unit/DateLozengeSpec.tsx
+++ packages/elements/date/src/__tests__/components/unit/DateLozengeSpec.tsx
@@ -3,48 +3,44 @@ import { colors } from '@atlaskit/theme';
 
 describe('resolveColors', () => {
   it('return default colors', () => {
-    expect(resolveColors()).toEqual([colors.N30A, colors.N800, colors.N40]);
-
-    expect(resolveColors('grey')).toEqual([
-      colors.N30A,
-      colors.N800,
-      colors.N40,
-    ]);
+    expect(resolveColors()).toEqual({
+      light: [colors.N30A, colors.N800, colors.N40],
+      dark: [colors.DN70, colors.DN800, colors.DN60],
+    });
   });
 
   it('return red colors', () => {
-    expect(resolveColors('red')).toEqual([colors.R50, colors.R500, colors.R75]);
+    expect(resolveColors('red')).toEqual({
+      light: [colors.R50, colors.R500, colors.R75],
+      dark: [colors.R50, colors.R500, colors.R75],
+    });
   });
 
   it('return blue colors', () => {
-    expect(resolveColors('blue')).toEqual([
-      colors.B50,
-      colors.B500,
-      colors.B75,
-    ]);
+    expect(resolveColors('blue')).toEqual({
+      light: [colors.B50, colors.B500, colors.B75],
+      dark: [colors.B50, colors.B500, colors.B75],
+    });
   });
 
   it('return green colors', () => {
-    expect(resolveColors('green')).toEqual([
-      colors.G50,
-      colors.G500,
-      colors.G75,
-    ]);
+    expect(resolveColors('green')).toEqual({
+      light: [colors.G50, colors.G500, colors.G75],
+      dark: [colors.G50, colors.G500, colors.G75],
+    });
   });
 
   it('return purple colors', () => {
-    expect(resolveColors('purple')).toEqual([
-      colors.P50,
-      colors.P500,
-      colors.P75,
-    ]);
+    expect(resolveColors('purple')).toEqual({
+      light: [colors.P50, colors.P500, colors.P75],
+      dark: [colors.P50, colors.P500, colors.P75],
+    });
   });
 
   it('return yellow colors', () => {
-    expect(resolveColors('yellow')).toEqual([
-      colors.Y50,
-      colors.Y500,
-      colors.Y75,
-    ]);
+    expect(resolveColors('yellow')).toEqual({
+      light: [colors.Y50, colors.Y500, colors.Y75],
+      dark: [colors.Y50, colors.Y500, colors.Y75],
+    });
   });
 });
