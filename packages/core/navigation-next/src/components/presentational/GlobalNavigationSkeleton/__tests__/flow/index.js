3c86379178cfd28f555ca6c622edb5be845a982d
Will Mendes <willmendesneto@gmail.com>
2018-12-12 17:51:46 +1100

    feat(navigation-next): adding global navigation skeleton

---
+++ packages/core/navigation-next/src/components/presentational/GlobalNavigationSkeleton/__tests__/flow/index.js
@@ -0,0 +1,9 @@
+// // @flow
+
+// import React from 'react';
+// import GlobalNavigationSkeleton from '../../index';
+
+// <GlobalNavigationSkeleton />;
+
+// // $ExpectError - invalid prop
+// <GlobalNavigationSkeleton type={'foo'} />;
