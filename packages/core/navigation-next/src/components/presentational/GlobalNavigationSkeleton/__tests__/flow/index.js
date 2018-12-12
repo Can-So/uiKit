180e80a9e710a9abde7ae6d9b09db356851fb855
Will Mendes <willmendesneto@gmail.com>
2018-12-12 18:04:08 +1100

    feat(navigation-next): adding global navigation skeleton as an entry point component

--- packages/core/navigation-next/src/components/presentational/GlobalNavigationSkeleton/__tests__/flow/index.js
+++
@@ -1,9 +0,0 @@
-// // @flow
-
-// import React from 'react';
-// import GlobalNavigationSkeleton from '../../index';
-
-// <GlobalNavigationSkeleton />;
-
-// // $ExpectError - invalid prop
-// <GlobalNavigationSkeleton type={'foo'} />;
