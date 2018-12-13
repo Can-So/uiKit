5e9f536407bf4fa8f02961c0dfa815b300d20452
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-12-13 21:19:13 +1100

    Add collectionName: descriptior.collection back

--- packages/media/media-test-helpers/src/mocks/routers/api-router.ts
+++ packages/media/media-test-helpers/src/mocks/routers/api-router.ts
@@ -282,6 +282,7 @@ export function createApiRouter(): Router<DatabaseSchema> {
     const records = descriptors.map((descriptor: any) => {
       const record = database.findOne('collectionItem', {
         id: descriptor.id,
+        collectionName: descriptor.collection,
       });
       return {
         type: 'file',
