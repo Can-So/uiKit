5180f37fc55e6cbf004b5189806e73005eee59f9
raja07 <rbellebon@atlassian.com>
2018-12-14 16:46:43 +1100

    TODO

--- packages/core/datetime-picker/src/internal/parseTime.js
+++ packages/core/datetime-picker/src/internal/parseTime.js
@@ -75,9 +75,9 @@ export function assignToDate(time: { hour: number, minute: number }): Date {
   return dateTime;
 }
 
-export default function(time: string) {
-  if (!isValid(time)) return 'invalid time format';
-  const time1 = removeSpacer(time);
+export default function(time: string | number): string | Date {
+  if (!isValid(time.toString())) return 'invalid time format';
+  const time1 = removeSpacer(time.toString());
   const time2 = convertTo24hrTime(time1);
   if (!time2) return 'invalid time format';
   return assignToDate(time2);
