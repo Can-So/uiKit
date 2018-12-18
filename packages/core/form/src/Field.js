55860803826d60cae9abf19f513611c0a6350d6f
Pete Gleeson <pgleeson@atlassian.com>
2018-12-18 17:46:01 +1100

    makes FieldProps flow type more specific

--- packages/core/form/src/Field.js
+++ packages/core/form/src/Field.js
@@ -12,11 +12,14 @@ import translateEvent from './utils/translateEvent';
 type FieldProps = {
   id: string,
   isRequired: boolean,
+  isDisabled: boolean,
   isInvalid: boolean,
   onChange: any => any,
   onBlur: () => any,
   onFocus: () => any,
   value: any,
+  'aria-invalid': 'true' | 'false',
+  'aria-labelledby': string,
 };
 
 type Meta = {
