3b1de17c8ca000805eca9b2f7c112898ac74ba8a
esoares <esoares@atlassian.com>
2018-12-21 10:10:47 +1100

    CHANGESET: 939f50b3. added i18n translated files

---
+++ packages/elements/mention/src/i18n/en.ts
@@ -0,0 +1,10 @@
+// English
+export default {
+  'fabric.mention.noAccess.warning':
+    "{name} won't be notified as they have no access",
+  'fabric.mention.noAccess.label': 'No access',
+  'fabric.mention.error.defaultHeadline': 'Something went wrong',
+  'fabric.mention.error.defaultAction': 'Try again in a few seconds',
+  'fabric.mention.error.loginAgain': 'Try logging out then in again',
+  'fabric.mention.error.differentText': 'Try entering different text',
+};
