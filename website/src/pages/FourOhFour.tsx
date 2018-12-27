cd3fd4b714c29584a313b4e788c515e8e04df70a
Ajay Narain Mathur <amathur@atlassian.com>
2018-12-27 11:49:22 +1100

    refresh the page once when 404 component is mounted

--- website/src/pages/FourOhFour.tsx
+++ website/src/pages/FourOhFour.tsx
@@ -6,6 +6,16 @@ export type FourOhFourProps = {};
 export default class FourOhFour extends React.PureComponent<FourOhFourProps> {
   props: FourOhFourProps;
 
+  componentDidMount() {
+    if (
+      sessionStorage.getItem('loadedOnce') === null ||
+      sessionStorage.getItem('loadedOnce') === 'false'
+    ) {
+      sessionStorage.setItem('loadedOnce', 'true');
+      window.location.reload();
+    }
+  }
+
   render() {
     return (
       <Page>
