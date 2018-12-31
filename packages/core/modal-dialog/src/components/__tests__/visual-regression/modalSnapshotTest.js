25663b3017daae8fbda3a567dd15902166355b9a
raja07 <rbellebon@atlassian.com>
2018-12-31 14:20:55 +1100

    Add select, onboarding and modal-dialog

---
+++ packages/core/modal-dialog/src/components/__tests__/visual-regression/modalSnapshotTest.js
@@ -0,0 +1,29 @@
+// @flow
+import {
+  getExampleUrl,
+  takeElementScreenShot,
+} from '@atlaskit/visual-regression/helper';
+
+const openModalBtn = "[type='button']";
+const modalDialog = "[role='dialog']";
+
+describe('Snapshot Test', () => {
+  it('Basic example should match production example', async () => {
+    const url = getExampleUrl(
+      'core',
+      'modal-dialog',
+      'basic',
+      global.__BASEURL__,
+    );
+    const { page } = global;
+
+    await page.goto(url);
+    await page.waitForSelector(openModalBtn);
+    await page.click(openModalBtn);
+    await page.waitFor(modalDialog);
+
+    const image = await takeElementScreenShot(page, modalDialog);
+    //$FlowFixMe
+    expect(image).toMatchProdImageSnapshot();
+  });
+});
