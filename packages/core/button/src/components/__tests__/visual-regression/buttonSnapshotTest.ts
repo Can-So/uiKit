ba7ecea29cdc237947d95c7f671cd87ab2419927
raja <rbellebon@atlassian.com>
2019-01-04 15:37:44 +1100

    Try to launch puppeteer from the test

--- packages/core/button/src/components/__tests__/visual-regression/buttonSnapshotTest.ts
+++ packages/core/button/src/components/__tests__/visual-regression/buttonSnapshotTest.ts
@@ -2,9 +2,17 @@ import {
   getExampleUrl,
   takeScreenShot,
 } from '@atlaskit/visual-regression/helper';
+const puppeteer = require('puppeteer');
 
 describe('Snapshot Test', () => {
   it('ButtonAppearances should match production example', async () => {
+    const browser = await puppeteer.launch({
+      // run test in headless mode
+      headless: true,
+      executablePath: process.env.CHROME_BIN || null,
+      slowMo: 100,
+      args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+    });
     const url = getExampleUrl(
       'core',
       'button',
@@ -13,7 +21,7 @@ describe('Snapshot Test', () => {
       global.__BASEURL__,
     );
     // @ts-ignore
-    const image = await takeScreenShot(global.page, url);
+    const image = await takeScreenShot(browser, url);
     expect(image).toMatchProdImageSnapshot();
   });
 });
