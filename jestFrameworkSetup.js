e041f4d68b06e215e5880ccd899eba3f23725e74
Raja Bellebon <rbellebon@atlassian.com>
2019-01-07 04:42:58 +0000

    jestFrameworkSetup.js edited online with Bitbucket

--- jestFrameworkSetup.js
+++ jestFrameworkSetup.js
@@ -348,9 +348,9 @@ if (process.env.VISUAL_REGRESSION) {
     global.page = await global.browser.newPage();
   }, jasmine.DEFAULT_TIMEOUT_INTERVAL);
 
-  afterAll(async () => {
-    await global.browser.close();
-  });
+  // afterAll(async () => {
+    // await global.browser.close();
+  // });
 
   // TODO tweak failureThreshold to provide best results
   // TODO: A failureThreshold of 1 will pass tests that have > 2 percent failing pixels
