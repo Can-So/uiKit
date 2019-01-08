8a40c43f2f1c50372feb1ec52bc14482cf83b8cd
raja <rbellebon@atlassian.com>
2019-01-08 11:57:08 +1100

    Update Select test to avoid forEach and skip Edge

--- packages/core/select/src/components/__tests__/integration/select.js
+++ packages/core/select/src/components/__tests__/integration/select.js
@@ -8,25 +8,58 @@ const urlSelect = getExampleUrl('core', 'select');
 const selectDefault = '.react-select__control';
 const selectMenu = '.react-select__menu';
 
-const urlArray = [
-  'single-select',
-  'multi-select',
-  'radio-select',
-  'async-select-with-callback',
-];
+BrowserTestCase(
+  `select.js: Single-select should display a menu once clicked and not throwing errors`,
+  { skip: ['edge'] },
+  async client => {
+    const selectTest = new Page(client);
+    await selectTest.goto(`${urlSelect}single-select`);
+    await selectTest.waitForSelector(selectDefault);
+    await selectTest.click(selectDefault);
+    const menuIsVisible = await selectTest.isVisible(selectMenu);
+    expect(menuIsVisible).toBe(true);
+    await selectTest.checkConsoleErrors();
+  },
+);
 
-urlArray.forEach(url => {
-  BrowserTestCase(
-    `select.js: ${url.toUpperCase()} should display a menu once clicked and not throwing errors`,
-    { skip: ['firefox'] },
-    async client => {
-      const selectTest = new Page(client);
-      await selectTest.goto(urlSelect + url);
-      await selectTest.waitForSelector(selectDefault);
-      await selectTest.click(selectDefault);
-      const menuIsVisible = await selectTest.isVisible(selectMenu);
-      expect(menuIsVisible).toBe(true);
-      await selectTest.checkConsoleErrors();
-    },
-  );
-});
+BrowserTestCase(
+  `select.js: Multi-select should display a menu once clicked and not throwing errors`,
+  { skip: ['edge'] },
+  async client => {
+    const selectTest = new Page(client);
+    await selectTest.goto(`${urlSelect}multi-select`);
+    await selectTest.waitForSelector(selectDefault);
+    await selectTest.click(selectDefault);
+    const menuIsVisible = await selectTest.isVisible(selectMenu);
+    expect(menuIsVisible).toBe(true);
+    await selectTest.checkConsoleErrors();
+  },
+);
+
+BrowserTestCase(
+  `select.js: Radio-select should display a menu once clicked and not throwing errors`,
+  { skip: ['edge'] },
+  async client => {
+    const selectTest = new Page(client);
+    await selectTest.goto(`${urlSelect}radio-select`);
+    await selectTest.waitForSelector(selectDefault);
+    await selectTest.click(selectDefault);
+    const menuIsVisible = await selectTest.isVisible(selectMenu);
+    expect(menuIsVisible).toBe(true);
+    await selectTest.checkConsoleErrors();
+  },
+);
+
+BrowserTestCase(
+  `select.js: Async-select should display a menu once clicked and not throwing errors`,
+  { skip: ['edge'] },
+  async client => {
+    const selectTest = new Page(client);
+    await selectTest.goto(`${urlSelect}async-select-with-callback`);
+    await selectTest.waitForSelector(selectDefault);
+    await selectTest.click(selectDefault);
+    const menuIsVisible = await selectTest.isVisible(selectMenu);
+    expect(menuIsVisible).toBe(true);
+    await selectTest.checkConsoleErrors();
+  },
+);
