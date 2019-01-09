0cc161ed5b575e6a426363965d5893a9c385af41
John Van Der Loo <jvanderloo@atlassian.com>
2019-01-09 13:45:33 +1100

    AK-5587: Unskip SSR tests related to theme

--- packages/core/code/src/__tests__/unit/server-side.js
+++ packages/core/code/src/__tests__/unit/server-side.js
@@ -6,9 +6,7 @@ import React from 'react';
 import { getExamplesFor } from '@atlaskit/build-utils/getExamples';
 import ReactDOMServer from 'react-dom/server';
 
-/* AK-5587: SSR tests failing because of theme */
-/* eslint-disable jest/no-disabled-tests */
-test.skip('Code server side rendering', async () => {
+test('Code server side rendering', async () => {
   (await getExamplesFor('code')).forEach(examples => {
     // $StringLitteral
     const Example = require(examples.filePath).default; // eslint-disable-line import/no-dynamic-require
