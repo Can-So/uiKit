ab67e21df6ef01fc605ea6956fbea68ac391c8c5
Ishan Somasiri <isomasiri@atlassian.com>
2019-01-11 11:49:47 +1100

    TEAMS-169 : Fixed incorrect imports

--- packages/elements/user-picker/examples/06-multi-with-default-values.tsx
+++ packages/elements/user-picker/examples/06-multi-with-default-values.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { exampleUsers } from '../example-helpers';
+import { exampleOptions } from '../example-helpers';
 import { ExampleWrapper } from '../example-helpers/ExampleWrapper';
 import { UserPicker } from '../src/components/UserPicker';
 
@@ -13,7 +13,7 @@ export default class Example extends React.Component<{}> {
             onChange={console.log}
             onInputChange={onInputChange}
             isMulti
-            defaultValue={[exampleUsers[0], exampleUsers[1]]}
+            defaultValue={[exampleOptions[0], exampleOptions[1]]}
           />
         )}
       </ExampleWrapper>
--- packages/elements/user-picker/examples/07-multi-with-fixed-values.tsx
+++ packages/elements/user-picker/examples/07-multi-with-fixed-values.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { exampleUsers } from '../example-helpers';
+import { exampleOptions } from '../example-helpers';
 import { ExampleWrapper } from '../example-helpers/ExampleWrapper';
 import { UserPicker } from '../src/components/UserPicker';
 
@@ -14,8 +14,8 @@ export default class Example extends React.Component<{}> {
             onInputChange={onInputChange}
             isMulti
             defaultValue={[
-              { ...exampleUsers[0], fixed: true },
-              { ...exampleUsers[1], fixed: true },
+              { ...exampleOptions[0], fixed: true },
+              { ...exampleOptions[1], fixed: true },
             ]}
           />
         )}
