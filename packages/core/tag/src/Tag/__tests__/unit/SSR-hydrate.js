4d74753ebc20f2924109f0544d425367a9d3d6bf
Ajay Mathur <amathur@atlassian.com>
2019-01-13 22:40:03 +0000

    SSR-hydrate.js edited online with Bitbucket

--- packages/core/tag/src/Tag/__tests__/unit/SSR-hydrate.js
+++ packages/core/tag/src/Tag/__tests__/unit/SSR-hydrate.js
@@ -28,6 +28,6 @@ test('should ssr then hydrate tag correctly', () => {
   canUseDom.mockReturnValue(true);
   const elem = document.createElement('div');
   elem.innerHTML = serverHTML;
-  expect(() => ReactDOM.hydrate(<App />, elem)).not.toThrow();
+  ReactDOM.hydrate(<App />, elem);
   expect(console.error).not.toBeCalled();
 });
