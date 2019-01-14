fe7db07e825b9a1f994964f6b1c7f28d0e24dd00
Marco de Jongh <mdejongh@atlassian.com>
2019-01-14 16:36:05 +1100

    ED-6037 Check-in forgotten files

---
+++ packages/editor/adf-utils/src/validator/specs/annotation.ts
@@ -0,0 +1,15 @@
+export default {
+  props: {
+    type: { type: 'enum', values: ['annotation'] },
+    attrs: {
+      props: {
+        id: { type: 'string' },
+        annotationType: {
+          type: 'enum',
+          values: ['inlineComment'],
+          optional: true,
+        },
+      },
+    },
+  },
+};
