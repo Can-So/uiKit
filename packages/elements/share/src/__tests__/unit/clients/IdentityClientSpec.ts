6b50fbfa8618a7f9e95cf2185fc532c0a0ee901a
Marston Ng <mng@atlassian.com>
2019-01-16 11:45:55 +1100

    Updated base on code review comments
    - added support for configurable serviceConfig
    - updated getInvitationsCapabilities to return the Promise itself
    - updated type definitions

--- packages/elements/share/src/__tests__/unit/clients/IdentityClientSpec.ts
+++ packages/elements/share/src/__tests__/unit/clients/IdentityClientSpec.ts
@@ -2,8 +2,8 @@ import { utils } from '@atlaskit/util-service-support';
 import {
   IdentityClientImpl,
   IdentityClient,
-  INVITATIONS_CAPABILITIES_PATH,
-  ID_PUBLIC_FACADE_URL,
+  DEFAULT_INVITATIONS_CAPABILITIES_PATH,
+  DEFAULT_ID_PUBLIC_FACADE_URL,
 } from '../../../clients/IdentityClient';
 
 const mockCloudId = 'mockCloudId';
@@ -13,7 +13,7 @@ describe('IdentityClientImpl', () => {
   let identityClient: IdentityClient;
 
   beforeEach(() => {
-    requestSpy = jest.spyOn(utils, 'requestService');
+    requestSpy = jest.spyOn(utils, 'requestService').mockResolvedValue({});
     identityClient = new IdentityClientImpl(mockCloudId);
   });
 
@@ -26,28 +26,26 @@ describe('IdentityClientImpl', () => {
       await identityClient.getInvitationsCapabilities();
       expect(requestSpy).toBeCalledTimes(1);
       expect(requestSpy.mock.calls[0][0]).toMatchObject({
-        url: ID_PUBLIC_FACADE_URL,
+        url: DEFAULT_ID_PUBLIC_FACADE_URL,
       });
       expect(requestSpy.mock.calls[0][1]).toMatchObject({
-        path: INVITATIONS_CAPABILITIES_PATH,
+        path: DEFAULT_INVITATIONS_CAPABILITIES_PATH,
         queryParams: {
           resource: `ari:cloud:platform::site/${mockCloudId}`,
         },
       });
     });
 
-    it('should return an error instance if requestService throws an error', async () => {
-      const mockError = new Error('Bad Request');
-      requestSpy.mockRejectedValue(mockError);
-      const result = await identityClient.getInvitationsCapabilities();
-      expect(result).toEqual(mockError);
-    });
-
-    it('should return the value if requestService resolves a value', async () => {
-      const mockResult = {};
-      requestSpy.mockResolvedValue(mockResult);
-      const result = await identityClient.getInvitationsCapabilities();
-      expect(result).toEqual(mockResult);
+    it('should support calling requestService with configurable serviceConfig', async () => {
+      const mockServiceConfig = {
+        url: 'mockUrl',
+      };
+      identityClient = new IdentityClientImpl(mockCloudId, mockServiceConfig);
+      await identityClient.getInvitationsCapabilities();
+      expect(requestSpy).toBeCalledTimes(1);
+      expect(requestSpy.mock.calls[0][0]).toMatchObject({
+        url: mockServiceConfig.url,
+      });
     });
   });
 });
