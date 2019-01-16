75ea47923fddcbe21a2dd9addfbbe678d55401c3
Mario Maradiaga <mmaradiaga@atlassian.com>
2019-01-16 17:44:32 +1100

    Adding recent containers provider

--- packages/navigation/centralised-app-switcher/src/providers/jira-data-providers.ts
+++ packages/navigation/centralised-app-switcher/src/providers/jira-data-providers.ts
@@ -1,17 +1,6 @@
 import { fetchJson } from '../utils/fetch';
 import asDataProvider, { DataProviderProps } from './as-data-provider';
 
-interface JiraCloudIdProvider extends DataProviderProps {
-  cloudId: string;
-}
-
-export const RecentContainersProvider = asDataProvider<JiraCloudIdProvider>(
-  ({ cloudId }) =>
-    fetchJson(
-      `/gateway/api/activity/api/client/recent/containers?cloudId=${cloudId}`,
-    ),
-);
-
 export const CustomLinksProvider = asDataProvider<DataProviderProps>(() =>
   Promise.all([
     fetchJson(`/rest/menu/latest/appswitcher`),
