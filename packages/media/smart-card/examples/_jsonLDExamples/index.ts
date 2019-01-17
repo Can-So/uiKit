a1ebac1fe958c392aca5bb421557bfd0dffd59cf
Abhi Kishore <akishore@atlassian.com>
2019-01-17 14:47:05 +1100

    feat(smart-cards): added icons for Confluence links.

--- packages/media/smart-card/examples/_jsonLDExamples/index.ts
+++ packages/media/smart-card/examples/_jsonLDExamples/index.ts
@@ -3,3 +3,10 @@ export {
   GithubRepository,
 } from './atlassian.sourceCodeRepository';
 export { AsanaTask, GitHubIssue } from './atlassian.task';
+
+// All Confluence mocks - mapped to different objects in
+// the Atlassian Vocabulary.
+export { ConfluencePage } from './atlassian.textDigitalDocument';
+export { ConfluenceSpace } from './atlassian.project';
+export { ConfluenceTemplate } from './atlassian.template';
+export { ConfluenceBlogPost } from './schema.blogpost';
