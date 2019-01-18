7b03055dfb7d8e3a0def190aa8d20b1e144db6bc
Chethana Krishnakumar <ckrishnakumar@c02t70qdh03y.office.atlassian.com>
2019-01-18 17:04:16 +1100

    pre-push script

--- append-to-existing-hooks.sh
+++ append-to-existing-hooks.sh
@@ -5,14 +5,14 @@ cp .git/hooks/pre-push .git/hooks/pre-push.bk
 cp .git/hooks/post-checkout .git/hooks/post-checkout.bk
 cp .git/hooks/post-commit .git/hooks/post-commit.bk
 cp .git/hooks/post-merge .git/hooks/post-merge.bk
-echo "command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/pre-push.\n"; exit 2; }
+echo "command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path.  Refer https://product-fabric.atlassian.net/wiki/spaces/FBT/pages/864979766/Enabling+git-lfs+on+Atlaskit for setup"; exit 2; }
 	git lfs pre-push "$@"" >> .git/hooks/pre-push
 
-echo "command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/post-checkout.\n"; exit 2; }
+echo "command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path.  Refer https://product-fabric.atlassian.net/wiki/spaces/FBT/pages/864979766/Enabling+git-lfs+on+Atlaskit for setup"; exit 2; }
 	git lfs post-checkout "$@"" >> .git/hooks/post-checkout
 
-echo "command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/post-commit.\n"; exit 2; }
+echo "command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. Refer https://product-fabric.atlassian.net/wiki/spaces/FBT/pages/864979766/Enabling+git-lfs+on+Atlaskit for setup"; exit 2; }
 	git lfs post-commit "$@"" >> .git/hooks/post-commit
 
-echo "command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/post-merge.\n"; exit 2; }
-	git lfs post-merge "$@"" >> .git/hooks/post-merge
+echo "command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. Refer https://product-fabric.atlassian.net/wiki/spaces/FBT/pages/864979766/Enabling+git-lfs+on+Atlaskit for setup"; exit 2; }
+	git lfs post-merge "$@" " >> .git/hooks/post-merge
