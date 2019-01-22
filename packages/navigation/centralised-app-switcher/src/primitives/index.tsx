bb54afdf5d2e84b1c38b694bb9d4e2ce61576e8e
Harsha Ivaturi <isriharsha@atlassian.com>
2019-01-22 22:30:04 +0530

    Clean up unused files. Fix examples

--- packages/navigation/centralised-app-switcher/src/primitives/index.tsx
+++ packages/navigation/centralised-app-switcher/src/primitives/index.tsx
@@ -2,12 +2,5 @@ import AppSwitcherItem from './item';
 import Section from './section';
 import AppSwitcherWrapper from './wrapper';
 import ManageButton from './manage-button';
-import WithBackground from './with-background';
 
-export {
-  AppSwitcherItem,
-  Section,
-  AppSwitcherWrapper,
-  ManageButton,
-  WithBackground,
-};
+export { AppSwitcherItem, Section, AppSwitcherWrapper, ManageButton };
