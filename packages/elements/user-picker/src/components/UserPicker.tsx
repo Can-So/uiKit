4bb3998b0d9b48b34fbd984a30769eb3e20fd33b
Luiz Pereira <lpereira@atlassian.com>
2019-01-23 15:30:27 +1100

    FS-3498 fix @atlaskit/user-picker types

--- packages/elements/user-picker/src/components/UserPicker.tsx
+++ packages/elements/user-picker/src/components/UserPicker.tsx
@@ -39,10 +39,9 @@ import {
   optionToSelectableOptions,
 } from './utils';
 
-class UserPickerInternal extends React.Component<
-  UserPickerProps & WithAnalyticsEventProps,
-  UserPickerState
-> {
+type Props = UserPickerProps & WithAnalyticsEventProps;
+
+class UserPickerInternal extends React.Component<Props, UserPickerState> {
   static defaultProps: UserPickerProps = {
     width: 350,
     isMulti: false,
@@ -77,11 +76,11 @@ class UserPickerInternal extends React.Component<
     return derivedState;
   }
 
-  private selectRef;
+  private selectRef: any | null;
 
   private session?: UserPickerSession;
 
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
     this.state = {
       options: [],
@@ -157,7 +156,7 @@ class UserPickerInternal extends React.Component<
     }
   };
 
-  private handleSelectRef = ref => {
+  private handleSelectRef = (ref: any | null) => {
     this.selectRef = ref;
   };
 
@@ -168,7 +167,10 @@ class UserPickerInternal extends React.Component<
           return {
             options: options.concat(
               newOptions.reduce<OptionData[]>(
-                (nextOptions, item) => nextOptions.concat(item[0]),
+                (nextOptions, item) =>
+                  Array.isArray(item)
+                    ? nextOptions.concat(item[0])
+                    : nextOptions.concat(item),
                 [],
               ),
             ),
@@ -193,7 +195,7 @@ class UserPickerInternal extends React.Component<
         const addOptions = this.addOptions.bind(
           this,
           inflightRequest.toString(),
-        );
+        ) as (value: OptionData | OptionData[]) => void | PromiseLike<void>;
         let count = 0;
         if (isIterable(result)) {
           for (const value of result) {
@@ -265,7 +267,7 @@ class UserPickerInternal extends React.Component<
     this.fireEvent(focusEvent);
   };
 
-  componentDidUpdate(prevProps: UserPickerProps, prevState: UserPickerState) {
+  componentDidUpdate(_: UserPickerProps, prevState: UserPickerState) {
     const { menuIsOpen, options } = this.state;
     // load options when the picker open
     if (menuIsOpen && !prevState.menuIsOpen) {
@@ -342,7 +344,6 @@ class UserPickerInternal extends React.Component<
 
   render() {
     const {
-      width,
       isMulti,
       anchor,
       isLoading,
@@ -363,6 +364,7 @@ class UserPickerInternal extends React.Component<
       inputValue,
     } = this.state;
     const appearance = this.getAppearance();
+    const width = this.props.width as string | number;
 
     return (
       <Select
