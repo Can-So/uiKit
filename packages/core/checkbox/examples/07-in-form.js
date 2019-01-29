aabb6cf4d3f379cad7c232f4d24a49a92a922cf1
Ajay Narain Mathur <amathur@atlassian.com>
2019-01-29 12:15:45 +1100

    updated the rachet as there is increase of 154 bytes due to addition of required asterisk in the checkbox

--- packages/core/checkbox/examples/07-in-form.js
+++ packages/core/checkbox/examples/07-in-form.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '@atlaskit/button';
 import Form, { CheckboxField, FormFooter } from '@atlaskit/form';
 import { Checkbox } from '../src';
