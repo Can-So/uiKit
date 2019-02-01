0507abeec61e9fdbdd5b8e007bafdb32ad0c7715
Marston Ng <mng@atlassian.com>
2019-02-01 15:10:57 +1100

    fixed typo in src/index.ts

--- packages/elements/share/src/index.ts
+++ packages/elements/share/src/index.ts
@@ -1,3 +1,3 @@
-export { ShareDialogContainer } from './component/ShareDialogContainer';
+export { ShareDialogContainer } from './components/ShareDialogContainer';
 export { ShareDialogWithTrigger } from './components/ShareDialogWithTrigger';
 export * from './types';
