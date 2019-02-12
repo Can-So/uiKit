7c105b5c3f693f3ec54800340b28e2f2a74c270c
Joss Mackison <jmackison@atlassian.com>
2019-02-12 16:46:37 +1100

    spread components onto CheckboxSelect props

--- packages/core/select/src/CheckboxSelect.js
+++ packages/core/select/src/CheckboxSelect.js
@@ -5,12 +5,12 @@ import React from 'react';
 import Select from './Select';
 import { CheckboxOption } from './components/input-options';
 
-const CheckboxSelect = (props: any) => (
+const CheckboxSelect = ({ components, ...props }: any) => (
   <Select
     closeMenuOnSelect={false}
     hideSelectedOptions={false}
     isMulti
-    components={{ Option: CheckboxOption }}
+    components={{ ...components, Option: CheckboxOption }}
     {...props}
   />
 );
