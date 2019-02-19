ccfc180f9935d4f2c2a2a6956f1ae0e6fa016a8d
Rodrigo Vieira <rvieira@atlassian.com>
2019-02-20 10:45:34 +1100

    Make sure width & height on media is a Number

--- packages/editor/adf-schema/src/__tests__/unit/schema/nodes/media.ts
+++ packages/editor/adf-schema/src/__tests__/unit/schema/nodes/media.ts
@@ -65,6 +65,24 @@ describe(`${name}/schema media node`, () => {
     expect(mediaNode.attrs.__displayType).toEqual(null);
   });
 
+  it('should parse width/height as number', () => {
+    const doc = fromHTML(
+      `
+    <div
+      data-node-type="media"
+      data-width="456"
+      data-height="956"
+    />
+    `,
+      schema,
+    );
+    const mediaGroup = doc.firstChild!;
+    const mediaNode = mediaGroup.firstChild!;
+
+    expect(mediaNode.attrs.width).toEqual(456);
+    expect(mediaNode.attrs.height).toEqual(956);
+  });
+
   it('should parse html (with occurrenceKey)', () => {
     const doc = fromHTML(
       `
@@ -223,6 +241,26 @@ describe(`${name}/schema media node`, () => {
         id: 'id',
         type: 'file',
         width: 0,
+      },
+    });
+  });
+
+  it('should serialize width/height as number', () => {
+    const mediaNode = schema.nodes.media.create({
+      id: 'id',
+      type: 'file',
+      collection: 'collection',
+      width: '100',
+      height: '150',
+    });
+
+    expect(toJSON(mediaNode)).toEqual({
+      attrs: {
+        collection: 'collection',
+        id: 'id',
+        type: 'file',
+        width: 100,
+        height: 150,
       },
     });
   });
