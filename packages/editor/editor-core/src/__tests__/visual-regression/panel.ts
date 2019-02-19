fb98fc658dc9213de13c646185afc6ab4915f3aa
ckrishnakumar <ckrishnakumar@atlassian.com>
2019-02-19 20:53:25 +1100

    update panel test

---
+++ packages/editor/editor-core/src/__tests__/visual-regression/panel.ts
@@ -0,0 +1,34 @@
+import { initFullPageEditorWithAdf, initCommentEditorWithAdf, snapshot, deviceViewPorts } from './_utils';
+import * as panel from './__fixtures__/panel-adf.json';
+
+describe('Snapshot Test: Panel overflow', () => {
+  let page;
+  beforeAll(() => {
+    // @ts-ignore
+    page = global.page;
+  })
+  it('looks correct for laptopMDPI', async () => {
+    await page.setViewport(deviceViewPorts.LaptopMDPI);
+    await initFullPageEditorWithAdf(page, panel);
+    await snapshot(page);
+  });
+
+  it('looks correct for ipad', async () => {
+    await page.setViewport(deviceViewPorts.iPad);
+    await initFullPageEditorWithAdf(page, panel);
+    await snapshot(page);
+  });
+
+  it('looks correct for iphone', async () => {
+    await page.setViewport(deviceViewPorts.iPhonePlus);
+    await initFullPageEditorWithAdf(page, panel);
+    await snapshot(page);
+  });
+
+  it('looks correct for LaptopMDPI in comment mode', async () => {
+    await page.setViewport(deviceViewPorts.LaptopMDPI);
+    await initCommentEditorWithAdf(page, panel);
+    await snapshot(page);
+  });
+
+});
