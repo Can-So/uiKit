21297f8ee2198e1d8098ad6ff6a9b93cdc703d82
Craig Petchell <cpetchell@atlassian.com>
2019-02-20 11:48:41 +1100

    FS-3333 - Fix status picker example to show stateful use case

--- packages/elements/status/examples/01-status-picker.tsx
+++ packages/elements/status/examples/01-status-picker.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
-import { StatusPicker } from '../src';
+import ManagedStatusPicker from '../example-helpers/ManagedStatusPicker';
 
 export default () => (
-  <div style={{ width: '225px' }}>
-    <StatusPicker
-      text={'In progress'}
-      selectedColor={'green'}
-      onTextChanged={t => console.log(`Text changed: ${t}`)}
-      onColorClick={c => console.log(`Color clicked: ${c}`)}
-      onEnter={() => console.log(`Enter pressed`)}
-    />
-  </div>
+  <ManagedStatusPicker
+    initialSelectedColor={'green'}
+    initialText={'In progress'}
+  />
 );
