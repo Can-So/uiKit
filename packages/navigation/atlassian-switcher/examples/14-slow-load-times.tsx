893f297301ea7a424d5051b9336b1bfb20e2cfca
George Burrows <gburrows@atlassian.com>
2019-02-24 20:38:59 +1100

    Add examples for slow response times

---
+++ packages/navigation/atlassian-switcher/examples/14-slow-load-times.tsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Button from '@atlaskit/button';
+import Drawer from '@atlaskit/drawer';
+import JiraSwitcher from '../src/components/jira-switcher';
+import { mockEndpoints } from './helpers/mock-endpoints';
+import { withAnalyticsLogger } from './helpers';
+
+class JiraSwitcherExample extends Component {
+  state = {
+    isDrawerOpen: false,
+  };
+
+  componentDidMount() {
+    this.openDrawer();
+  }
+
+  openDrawer = () => {
+    mockEndpoints('jira', undefined, {
+      containers: 1000,
+      xflow: 500,
+      licenseInformation: 250,
+      permitted: 2000,
+      appswitcher: 1500,
+    });
+    this.setState({
+      isDrawerOpen: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      isDrawerOpen: false,
+    });
+  };
+
+  onTriggerXFlow = (productKey: string) => {
+    console.log(`Triggering xflow for => ${productKey}`);
+  };
+
+  render() {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <Drawer onClose={this.onClose} isOpen={this.state.isDrawerOpen}>
+          <JiraSwitcher
+            cloudId="some-cloud-id"
+            triggerXFlow={this.onTriggerXFlow}
+          />
+        </Drawer>
+        <Button type="button" onClick={this.openDrawer}>
+          Open drawer
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default withAnalyticsLogger(JiraSwitcherExample);
