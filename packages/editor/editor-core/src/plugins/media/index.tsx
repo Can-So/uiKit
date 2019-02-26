martunia887/test198

ED-6061: support image annotations in editor

packages/editor/editor-core/src/plugins/media/index.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import EditorImageIcon from '@atlaskit/icon/glyph/editor/image';
 import { media, mediaGroup, mediaSingle } from '@atlaskit/adf-schema';
 import { EditorPlugin, EditorAppearance } from '../../types';
+import { SmartMediaEditor } from '@atlaskit/media-editor';
+import { FileIdentifier } from '@atlaskit/media-card';
 import {
   stateKey as pluginKey,
   createPlugin,
   MediaState,
   MediaStateManager,
   DefaultMediaStateManager,
+  MediaPluginState,
 } from './pm-plugins/main';
 import keymapMediaSinglePlugin from './pm-plugins/keymap-media-single';
 import keymapPlugin from './pm-plugins/keymap';
@@ -26,6 +29,7 @@ import {
   EVENT_TYPE,
   ACTION_SUBJECT_ID,
 } from '../analytics';
+import WithPluginState from '../../ui/WithPluginState';
 
 export {
   MediaState,
@@ -47,6 +51,34 @@ export interface MediaOptions {
 export interface MediaSingleOptions {
   disableLayout?: boolean;
 }
+
+export const renderSmartMediaEditor = (mediaState: MediaPluginState) => {
+  const node = mediaState.selectedMediaContainerNode();
+
+  if (node && mediaState.uploadContext && mediaState.showEditingDialog) {
+    const state = mediaState.getMediaNodeState(node.firstChild!.attrs.id);
+    const id = (state && state.fileId) || node.firstChild!.attrs.id;
+    const identifier: FileIdentifier = {
+      id,
+      mediaItemType: 'file',
+      collectionName: node.firstChild!.attrs.collection,
+    };
+
+    return (
+      <SmartMediaEditor
+        identifier={identifier}
+        context={mediaState.uploadContext}
+        onUploadStart={(newFileIdentifier: FileIdentifier) => {
+          mediaState.closeMediaEditor();
+          mediaState.replaceEditingMedia(newFileIdentifier);
+        }}
+        onFinish={mediaState.closeMediaEditor}
+      />
+    );
+  }
+
+  return null;
+};
 
 const mediaPlugin = (
   options?: MediaOptions,
@@ -124,6 +156,18 @@ const mediaPlugin = (
             plugin: ({ schema }) => keymapMediaSinglePlugin(schema),
           }
         : [],
+    );
+  },
+
+  contentComponent({ editorView }) {
+    return (
+      <WithPluginState
+        editorView={editorView}
+        plugins={{
+          mediaState: pluginKey,
+        }}
+        render={({ mediaState }) => renderSmartMediaEditor(mediaState)}
+      />
     );
   },
 

Can-So/uiKit

packages/editor/editor-core/src/plugins/media/index.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import EditorImageIcon from '@atlaskit/icon/glyph/editor/image';
 import { media, mediaGroup, mediaSingle } from '@atlaskit/adf-schema';
 import { EditorPlugin, EditorAppearance } from '../../types';
+import { SmartMediaEditor } from '@atlaskit/media-editor';
+import { FileIdentifier } from '@atlaskit/media-core';
 import {
   stateKey as pluginKey,
   createPlugin,
   MediaState,
   MediaStateManager,
   DefaultMediaStateManager,
+  MediaPluginState,
 } from './pm-plugins/main';
 import keymapMediaSinglePlugin from './pm-plugins/keymap-media-single';
 import keymapPlugin from './pm-plugins/keymap';
@@ -26,6 +29,7 @@ import {
   EVENT_TYPE,
   ACTION_SUBJECT_ID,
 } from '../analytics';
+import WithPluginState from '../../ui/WithPluginState';
 
 export {
   MediaState,
@@ -42,11 +46,40 @@ export interface MediaOptions {
   customDropzoneContainer?: HTMLElement;
   customMediaPicker?: CustomMediaPicker;
   allowResizing?: boolean;
+  allowAnnotation?: boolean;
 }
 
 export interface MediaSingleOptions {
   disableLayout?: boolean;
 }
+
+export const renderSmartMediaEditor = (mediaState: MediaPluginState) => {
+  const node = mediaState.selectedMediaContainerNode();
+
+  if (node && mediaState.uploadContext && mediaState.showEditingDialog) {
+    const state = mediaState.getMediaNodeState(node.firstChild!.attrs.id);
+    const id = (state && state.fileId) || node.firstChild!.attrs.id;
+    const identifier: FileIdentifier = {
+      id,
+      mediaItemType: 'file',
+      collectionName: node.firstChild!.attrs.collection,
+    };
+
+    return (
+      <SmartMediaEditor
+        identifier={identifier}
+        context={mediaState.uploadContext}
+        onUploadStart={(newFileIdentifier: FileIdentifier) => {
+          mediaState.closeMediaEditor();
+          mediaState.replaceEditingMedia(newFileIdentifier);
+        }}
+        onFinish={mediaState.closeMediaEditor}
+      />
+    );
+  }
+
+  return null;
+};
 
 const mediaPlugin = (
   options?: MediaOptions,
@@ -127,6 +160,18 @@ const mediaPlugin = (
     );
   },
 
+  contentComponent({ editorView }) {
+    return (
+      <WithPluginState
+        editorView={editorView}
+        plugins={{
+          mediaState: pluginKey,
+        }}
+        render={({ mediaState }) => renderSmartMediaEditor(mediaState)}
+      />
+    );
+  },
+
   secondaryToolbarComponent({ editorView, eventDispatcher, disabled }) {
     return (
       <ToolbarMedia
@@ -168,6 +213,7 @@ const mediaPlugin = (
         state,
         intl,
         options && options.allowResizing,
+        options && options.allowAnnotation,
         appearance,
       ),
   },