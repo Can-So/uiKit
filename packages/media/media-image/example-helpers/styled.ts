a6bbc948f97db9de5f946d6a1f76861f247b3f32
Will Mendes <willmendesneto@gmail.com>
2019-02-27 22:18:03 +1100

    chore(media-image): adding upgrade docs

--- packages/media/media-image/example-helpers/styled.ts
+++ packages/media/media-image/example-helpers/styled.ts
@@ -1,10 +1,10 @@
+import { colors } from '@atlaskit/theme';
 import styled from 'styled-components';
 
 export const OptionsWrapper = styled.div`
-  border-bottom: 1px solid #ccc;
+  border-bottom: 1px solid ${colors.N40};
   padding: 10px;
   margin: 10px auto;
-  width: 1000px;
 `;
 
 export const MediaImageWrapper = styled.div`
