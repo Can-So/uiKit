5d74808f361181f6d5db571f4ec92e8d1b5c3ecd
Will Mendes <willmendesneto@gmail.com>
2019-03-06 10:06:02 +1100

    chore: reverting code to use `_blank` only in IE and Safari

--- packages/media/media-core/src/file/index.ts
+++ packages/media/media-core/src/file/index.ts
@@ -342,6 +342,13 @@ export class FileFetcherImpl implements FileFetcher {
     name: string = 'download',
     collectionName?: string,
   ) {
+    const isIE11 =
+      !!(window as any).MSInputMethodContext &&
+      !!(document as any).documentMode;
+    const isSafari = /^((?!chrome|android).)*safari/i.test(
+      (navigator as Navigator).userAgent,
+    );
+
     const iframeName = 'media-download-iframe';
     const link = document.createElement('a');
     let iframe = document.getElementById(iframeName) as HTMLIFrameElement;
@@ -354,7 +361,7 @@ export class FileFetcherImpl implements FileFetcher {
     }
     link.href = await this.mediaStore.getFileBinaryURL(id, collectionName);
     link.download = name;
-    link.target = '_blank';
+    link.target = isIE11 || isSafari ? '_blank' : iframeName;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
