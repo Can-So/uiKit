5911430b6899c11ecab2a7b3a816a8b9fb7b3afa
Marston Ng <mng@atlassian.com>
2019-03-07 11:22:22 +1100

    exposed shareContentType prop to elements/share

--- packages/elements/share/src/components/ShareDialogContainer.tsx
+++ packages/elements/share/src/components/ShareDialogContainer.tsx
@@ -28,6 +28,7 @@ export type Props = {
   originTracingFactory: OriginTracingFactory;
   productId: string;
   shareAri: string;
+  shareContentType: string;
   shareLink: string;
   shareTitle: string;
   shouldCloseOnEscapePress?: boolean;
@@ -126,6 +127,7 @@ export class ShareDialogContainer extends React.Component<Props, State> {
       originTracingFactory,
       productId,
       shareAri,
+      shareContentType,
       shareLink,
       shareTitle,
     } = this.props;
@@ -134,6 +136,7 @@ export class ShareDialogContainer extends React.Component<Props, State> {
       // original share link is used here
       link: shareLink,
       title: shareTitle,
+      type: shareContentType,
     };
     const metaData: MetaData = {
       productId,
