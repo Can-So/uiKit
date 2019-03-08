f1842240ec050f632add298e0a4225833f4aa87c
Thiago de Bastos <thiago@thiagodebastos.com>
2019-03-08 11:01:37 +1100

    remove unecessary require logic

--- build/ssr/ssr.js
+++ build/ssr/ssr.js
@@ -1,11 +1,5 @@
 const path = require('path');
 
-require('@babel/register')({
-  plugins: [path.join(__dirname, 'require-transform.js')],
-  presets: ['@babel/preset-typescript'],
-  extensions: ['.js', '.jsx', '.ts', '.tsx'],
-});
-require('@babel/polyfill');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 
