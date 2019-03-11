7261577953c425608ed91e6973f1ed8469f292ee
esoares <esoares@atlassian.com>
2019-03-12 10:49:36 +1100

    CHANGESET: 78ab7e92. use @atlaskit/ssr to ssr/hydrate mention examples

--- packages/elements/mention/examples/11-resourced-mention-list-hydrate.tsx
+++ packages/elements/mention/examples/11-resourced-mention-list-hydrate.tsx
@@ -1,4 +1,4 @@
 import Example from './04-resourced-mention-list';
-import createHydrateExample from '../example-helpers/demo-ssr-hydrate';
+import createHydrateExample from '@atlaskit/elements-test-helpers/src/demo-ssr-hydrate';
 
 export default createHydrateExample(Example);
