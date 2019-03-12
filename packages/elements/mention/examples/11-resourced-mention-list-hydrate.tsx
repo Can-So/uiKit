72779c406076e0b1d7b8ce9229de171e84aa99e9
esoares <esoares@atlassian.com>
2019-03-12 17:46:49 +1100

    removed mention picker and mention list from ssr tests

--- packages/elements/mention/examples/11-resourced-mention-list-hydrate.tsx
+++
@@ -1,4 +0,0 @@
-import Example from './04-resourced-mention-list';
-import createHydrateExample from '@atlaskit/elements-test-helpers/src/demo-ssr-hydrate';
-
-export default createHydrateExample(Example);
--- packages/elements/mention/src/__tests__/unit/ssr/server-side-hydrate.tsx
+++ packages/elements/mention/src/__tests__/unit/ssr/server-side-hydrate.tsx
@@ -15,25 +15,9 @@ describe('server side rendering and hydration', async () => {
   test.each([
     ['00-simple-mention-item.tsx'],
     ['01-mention-item.tsx'],
-    ['03-error-mention-list.tsx'],
-    ['04-resourced-mention-list.tsx'],
-    ['05-mention-list-picker.tsx'],
-    ['06-mention-list-picker-with-slow-providers.tsx'],
     ['07-dark-simple-mention.tsx'],
     ['07-simple-mention.tsx'],
     ['08-resourced-mention-on-n20-background.tsx'],
-    ['09-mention-picker-external-asap.tsx'],
-    ['10-mention-picker-external-cookie.tsx'],
-  ])('ssr("%s")', async (fileName: string) => {
-    await ssr_hydrate(__dirname, `${ExamplesPath}/${fileName}`);
-
-    // tslint:disable-next-line:no-console
-    expect(console.error).not.toBeCalled();
-  });
-
-  // TODO: https://product-fabric.atlassian.net/browse/FS-3667
-  test.skip.each([
-    ['02-mention-list.tsx'], // Warning: Prop `className` did not match. Server: "sc-cMljjf gGGOqx" Client: "sc-cMljjf gQKxVT"
   ])('ssr("%s")', async (fileName: string) => {
     await ssr_hydrate(__dirname, `${ExamplesPath}/${fileName}`);
 
