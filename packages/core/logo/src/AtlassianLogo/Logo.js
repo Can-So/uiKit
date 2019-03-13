9d4cc0b7cc08c44921d915b25f45b1b55a263808
Thiago de Bastos <thiago@thiagodebastos.com>
2019-03-13 15:41:53 +1100

    removes unnecessary comment

--- packages/core/logo/src/AtlassianLogo/Logo.js
+++ packages/core/logo/src/AtlassianLogo/Logo.js
@@ -7,7 +7,6 @@ import { type Props, DefaultProps } from '../constants';
 import Wrapper from '../Wrapper';
 
 const svg = (iconGradientStart: string, iconGradientStop: string) => {
-  // this approach
   const id = uid({ iconGradientStart: iconGradientStop });
   return `<canvas height="32" width="190" aria-hidden="true"></canvas>
   <svg viewBox="0 0 190 32" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true">
