e33b9afe742c239762697433eb3c2ab2d41c0288
Thiago de Bastos <thiago@thiagodebastos.com>
2019-03-13 13:58:19 +1100

    replace uuid with react-uuid for ssr

--- packages/core/dropdown-menu/src/components/__tests__/unit/server-side-hydrate.js
+++ packages/core/dropdown-menu/src/components/__tests__/unit/server-side-hydrate.js
@@ -10,7 +10,7 @@ afterEach(() => {
   jest.resetAllMocks();
 });
 
-test.skip('should ssr then hydrate dropdown-menu correctly', async () => {
+test('should ssr then hydrate dropdown-menu correctly', async () => {
   const [example] = await getExamplesFor('dropdown-menu');
   // $StringLitteral
   const Example = require(example.filePath).default; // eslint-disable-line import/no-dynamic-require
