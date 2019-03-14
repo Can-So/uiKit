627322e8896c139a76f6c362c60ae7793b809583
Alex Hixon <ahixon@atlassian.com>
2019-03-14 16:01:42 +1100

    revert removing tests from e04c61d (via ED-6183)

---
+++ packages/editor/editor-core/src/__tests__/integration/media/copy-mediaSingle-replacement.ts
@@ -0,0 +1,111 @@
+import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
+import { editable, getDocFromElement, fullpage } from '../_helpers';
+import {
+  goToEditorTestingExample,
+  mountEditor,
+} from '../../__helpers/testing-example-helpers';
+
+const baseADF = {
+  version: 1,
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'asdasdas',
+        },
+      ],
+    },
+    {
+      type: 'mediaSingle',
+      attrs: {
+        width: 66.67,
+        layout: 'wrap-left',
+      },
+      content: [
+        {
+          type: 'media',
+          attrs: {
+            id: 'a559980d-cd47-43e2-8377-27359fcb905f',
+            type: 'file',
+            collection: 'MediaServicesSample',
+            width: '2378',
+            height: '628',
+          },
+        },
+      ],
+    },
+    {
+      type: 'mediaSingle',
+      attrs: {
+        width: 66.67,
+        layout: 'wrap-right',
+      },
+      content: [
+        {
+          type: 'media',
+          attrs: {
+            id: 'a559980d-cd47-43e2-8377-27359fcb905f',
+            type: 'file',
+            collection: 'MediaServicesSample',
+            width: '2378',
+            height: '628',
+          },
+        },
+      ],
+    },
+    {
+      type: 'mediaSingle',
+      attrs: {
+        width: 41.666666666666664,
+        layout: 'wrap-left',
+      },
+      content: [
+        {
+          type: 'media',
+          attrs: {
+            id: 'a559980d-cd47-43e2-8377-27359fcb905f',
+            type: 'file',
+            collection: 'MediaServicesSample',
+            width: '2378',
+            height: '628',
+          },
+        },
+      ],
+    },
+    {
+      type: 'paragraph',
+      content: [],
+    },
+  ],
+};
+
+BrowserTestCase(
+  'copy-mediaSingle-replacement.ts: Copies and pastes mediaSingle on fullpage',
+  { skip: ['edge', 'ie', 'safari'] },
+  async client => {
+    const page = await goToEditorTestingExample(client);
+    await mountEditor(page, {
+      appearance: fullpage.appearance,
+      defaultValue: JSON.stringify(baseADF),
+      media: {},
+    });
+    // select the middle one and copy it
+    //
+    // uses .overlay since these error without being signed into Media Services
+    // use the .wrapper selector if we're able to resolve the image
+    await page.waitForSelector('.ProseMirror :nth-child(3) .overlay');
+    await page.click('.ProseMirror :nth-child(3) .overlay');
+    await page.copy(editable);
+
+    // select the last one and replace it
+    await page.waitForSelector('.ProseMirror :nth-child(4) .overlay');
+    await page.click('.ProseMirror :nth-child(4) .overlay');
+    await page.paste(editable);
+
+    const doc = await page.$eval(editable, getDocFromElement);
+    expect(doc).toMatchDocSnapshot();
+  },
+);
